791e60d7895dc514740402dad7d01ea2b4080c4e
unknown <andrei_g_git@protonmail.com>
2022-06-17 07:20:14 +0300

    to avoid error: Your local changes to the following files would be overwritten by merge

--- src/components/showcaseOverlay/ShowcaseOverlay.js
+++ src/components/showcaseOverlay/ShowcaseOverlay.js
@@ -3,8 +3,10 @@ exports.__esModule = true;
 var React = require("react");
 require("./ShowcaseOverlay.scss");
 function ShowcaseOverlay(props) {
+    var isHovering = false;
     return (React.createElement("div", { className: props.darkTheme ? "theme-dark" : "theme-light" },
-        React.createElement("div", { className: "showcase-overlay-container" },
+        React.createElement("div", { className: "showcase-overlay-container" //{"showcase-overlay-container" + " " + isHovering? "visible" : ""}
+            , onMouseOver: function () { isHovering = true; } },
             React.createElement("h3", { className: "showcase-title" }, props.title),
             React.createElement("p", { className: "showcase-description" }, props.description),
             React.createElement("div", { className: "showcase-button", onClick: function () { props.notifyParent(props.index); } }, "EXPAND"))));
--- src/components/showcaseOverlay/ShowcaseOverlay.tsx
+++ src/components/showcaseOverlay/ShowcaseOverlay.tsx
@@ -2,25 +2,30 @@ import * as React from 'react';
 import "./ShowcaseOverlay.scss";
 
 function ShowcaseOverlay(props: any) { //switch dark/light theme
-  return (
-    <div className={props.darkTheme ? "theme-dark" : "theme-light"}>
-        <div className="showcase-overlay-container">
-            <h3 className="showcase-title">
-                {props.title}
-            </h3>
 
-            <p className="showcase-description">
-                {props.description}
-            </p>
+    let isHovering = false;
 
-            <div className="showcase-button"
-                onClick={() => {props.notifyParent(props.index)}}
+    return (
+        <div className={props.darkTheme ? "theme-dark" : "theme-light"}>
+            <div className="showcase-overlay-container"//{"showcase-overlay-container" + " " + isHovering? "visible" : ""}
+                onMouseOver={() => {isHovering = true}}
             >
-                EXPAND
+                <h3 className="showcase-title">
+                    {props.title}
+                </h3>
+
+                <p className="showcase-description">
+                    {props.description}
+                </p>
+
+                <div className="showcase-button"
+                    onClick={() => {props.notifyParent(props.index)}}
+                >
+                    EXPAND
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default ShowcaseOverlay;
